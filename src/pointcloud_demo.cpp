e49773aa85124ecda3d1ea3651e372ec4e8a967e
Neykah <morgan.lefranc@gmail.com>
2018-06-15 13:32:08 +0900

    Compilable base

--- src/pointcloud_demo.cpp
+++ src/pointcloud_demo.cpp
@@ -7,21 +7,9 @@
 int main(int argc, char** argv) {
     ros::init(argc, argv, "pointcloud_demo");
     ros::NodeHandle nh;
-    ros::Publisher crop_pub = 
-        nh.advertise<sensor_msgs::PointCloud2>("cropped_cloud", 1, true);
-    perception::Cropper cropper(crop_pub);
-    ros::Subscriber crop_sub = 
-        nh.subscribe("cloud_in", 1, &perception::Cropper::Callback, &cropper);
-    
-    // perception::Downsampler downsampler;
-    // ros::Subscriber down_sub = 
-    //     nh.subscribe("cropped_cloud", 1, &perception::Downsampler::Callback, &downsampler);
-
-    ros::Publisher table_pub = 
-        nh.advertise<sensor_msgs::PointCloud2>("table_cloud", 1, true);
+    ros::Publisher table_pub = nh.advertise<sensor_msgs::PointCloud2>("table_cloud", 1, true);
     perception::Segmenter segmenter(table_pub);
-    ros::Subscriber sub = 
-        nh.subscribe("cropped_cloud", 1, &perception::Segmenter::Callback, &segmenter);
+    ros::Subscriber sub = nh.subscribe("cloud_in",1, &perception::Segmenter::Callback, &segmenter);
     ros::spin();
     return 0;
 }
